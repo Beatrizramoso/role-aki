8e689bb8261b1b34d7c234f95be7cfeada149465
Beatriz <beatrizramoso2024@gmail.com>
2025-10-26 18:07:07 -0300

    initial commit

---
+++ role-aki/src/main/java/com/example/role_aki/RoleAkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.role_aki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleAkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleAkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/role_aki/RoleAkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.role_aki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleAkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
